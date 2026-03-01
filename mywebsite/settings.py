9368851e1bfcfdc70a9b0cf55de38ee0128125d5
mavin321 <mavinpeteromondi@gmail.com>
2026-03-01 22:09:52 +0300

    portfolio v17

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-afv(f_%%_35#m$allygkq$^n-iq*t%ly4h21bx6tj4#kx^-6*k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mavin321.pythonanywhere.com']
 
 
 # Application definition
